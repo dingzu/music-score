39db8c68720c469334328527efcefad175b781e5
D\\Z <dzwangyihan@gmail.com>
2025-02-21 07:14:48 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">首页</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="less" scoped>
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  nav {
+    padding: 1rem 0;
+    border-bottom: 1px solid #eee;
+    margin-bottom: 2rem;
+
+    a {
+      color: #2c3e50;
+      text-decoration: none;
+      margin-right: 1rem;
+
+      &.router-link-active {
+        color: #42b883;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>欢迎来到音乐评分系统</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="less" scoped>
+.home {
+  padding: 20px;
+  
+  h1 {
+    color: #333;
+    text-align: center;
+  }
+}
+</style> 
